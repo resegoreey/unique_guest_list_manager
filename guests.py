e6cae3325273f09ba1c95d697c0dc966d3aa306a
RESEGO MOTLHASI <remotlhjhb024@student.wethinkcode.co.za>
2025-02-19 21:04:28 +0200

    Added basic code to avoid duplicates

--- guests.py
+++ guests.py
@@ -2,11 +2,21 @@ guests = set()
 user_guests = input("Enter a guest(q to quit): ")
 
 while True:
+    #stops the program when the user enters q
     if user_guests.lower() == "q":
         print("You stopped adding guests, bye!")
         break
     else:
-        guests.add(user_guests)
-        print(guests)
+        if user_guests == "":
+            print("Guest name can't be empty")
+        else:
+        # Allows users to add guests to the list
+            guests.add(user_guests)
+            print(guests)
     user_guests = input("Enter a guest(q to quit): ")
-    
+
+    if user_guests in guests:
+        print(f"{user_guests} already exists")
+print() 
+
+
